c96bb23662db4f0709f7fc00939d5a371ac456e5
wangyang0210 <i@easybe.org>
2023-04-25 16:59:43 +0800

    fix

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -354,57 +354,57 @@ export default function main(_) {
      * 设置是否默认展开菜单栏
      */
     (() => {
-        if ($.__config.sidebar.submenu.pointsRank) {
+        if (_.__config.sidebar.submenu.pointsRank) {
             $('#sb-sidebarScorerank').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-sidebarScorerank').show()
         }
 
-        if ($.__config.sidebar.submenu.latestPosts) {
+        if (_.__config.sidebar.submenu.latestPosts) {
             $('#sb-sidebarRecentposts').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-sidebarRecentposts').show()
         }
 
-        if ($.__config.sidebar.submenu.myTags) {
+        if (_.__config.sidebar.submenu.myTags) {
             $('#sb-toptags').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-toptags').show()
         }
 
-        if ($.__config.sidebar.submenu.postsClassify) {
+        if (_.__config.sidebar.submenu.postsClassify) {
             $('#sb-classify').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-classify').show()
         }
 
-        if ($.__config.sidebar.submenu.articleClassify) {
+        if (_.__config.sidebar.submenu.articleClassify) {
             $('#sb-ArticleCategory').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-ArticleCategory').show()
         }
 
-        if ($.__config.sidebar.submenu.readRank) {
+        if (_.__config.sidebar.submenu.readRank) {
             $('#sb-topview').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-topview').show()
         }
 
-        if ($.__config.sidebar.submenu.recommendRank) {
+        if (_.__config.sidebar.submenu.recommendRank) {
             $('#sb-topDiggPosts').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-topDiggPosts').show()
         }
 
-        if ($.__config.sidebar.submenu.postsArchive) {
+        if (_.__config.sidebar.submenu.postsArchive) {
             $('#sb-record').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-record').show()
         }
 
-        if ($.__config.sidebar.submenu.articleArchive) {
+        if (_.__config.sidebar.submenu.articleArchive) {
             $('#sb-articlearchive').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-articlearchive').show()
         }
 
-        if ($.__config.sidebar.submenu.latestComment) {
+        if (_.__config.sidebar.submenu.latestComment) {
             $('#sb-recentComments').parent('li.sidebar-dropdown').addClass('active')
             $('#sb-recentComments').show()
         }
 
-        if ($.__config.sidebar.submenu.customList) {
+        if (_.__config.sidebar.submenu.customList) {
             $('#customize-sidebar-menu .sidebar-submenu').parent('li.sidebar-dropdown').addClass('active')
             $('#customize-sidebar-menu .sidebar-submenu').show()
         }
