8684196a5419d2faaa00923c2f8d495f10b3acf1
wangyang <i@easybe.org>
2024-06-14 01:16:40 +0800

    feat(link): link category and add animation

--- src/pages/links.js
+++ src/pages/links.js
@@ -11,7 +11,7 @@ import linksTemp from '../template/links.html';
 import articleDirectory from '../components/articleDirectory/articleDirectory';
 
 // 生成友链的html
-const generateLinkHtml = (link, index) => {
+const generateLinkHtml = (link, index, _) => {
     const { avatar = '', name = '', introduction = '', url = '' } = link;
     const icons = ['icon-zhifeiji', 'icon-like_fill', 'icon-flashlight_fill'];
     const icon = icons[index % icons.length];
@@ -25,10 +25,10 @@ const generateLinkHtml = (link, index) => {
 };
 
 // 生成完整的友链分类的html
-const generateSectionHtml = (data) => {
+const generateSectionHtml = (data, _) => {
     const { title, icon, style, links } = data;
     const sectionTitle = title ? `<h1 class="iconfont ${icon}" style="${style}">${title}</h1>` : '';
-    const linksHtml = links.map(generateLinkHtml).join('');
+    const linksHtml = links.map((link, index) => generateLinkHtml(link, index, _)).join('');
     return `${sectionTitle}<div id="links-box">${linksHtml}</div>`;
 };
 
@@ -47,7 +47,7 @@ export default function main(_) {
         if (_.__config.links.page.length) {
             const postBody = $('#cnblogs_post_body');
             const articleSuffixFlg = $('.articleSuffix-flg');
-            const linksHtml = _.__config.links.page.map(generateSectionHtml).join('');
+            const linksHtml = _.__config.links.page.map((data) => generateSectionHtml(data, _)).join('');
 
             // 插入模版
             articleSuffixFlg.length ? articleSuffixFlg.before(linksHtml) : postBody.append(linksHtml);
