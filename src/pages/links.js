05c538df1195306a9d8e316c4fb5635ab56580ad
wangyang <i@easybe.org>
2024-06-14 01:20:44 +0800

    feat(link): link category and add animation

--- src/pages/links.js
+++ src/pages/links.js
@@ -10,28 +10,6 @@ import '../style/links.css';
 import linksTemp from '../template/links.html';
 import articleDirectory from '../components/articleDirectory/articleDirectory';
 
-// 生成友链的html
-const generateLinkHtml = (link, index, _) => {
-    const { avatar = '', name = '', introduction = '', url = '' } = link;
-    const icons = ['icon-zhifeiji', 'icon-like_fill', 'icon-flashlight_fill'];
-    const icon = icons[index % icons.length];
-    return _.__tools.batchTempReplacement(linksTemp, [
-        ['avatar', avatar],
-        ['name', name],
-        ['introduction', introduction],
-        ['url', url],
-        ['icon', icon],
-    ]);
-};
-
-// 生成完整的友链分类的html
-const generateSectionHtml = (data, _) => {
-    const { title, icon, style, links } = data;
-    const sectionTitle = title ? `<h1 class="iconfont ${icon}" style="${style}">${title}</h1>` : '';
-    const linksHtml = links.map((link, index) => generateLinkHtml(link, index, _)).join('');
-    return `${sectionTitle}<div id="links-box">${linksHtml}</div>`;
-};
-
 export default function main(_) {
     /**
      * 文章页公共处理
@@ -47,7 +25,30 @@ export default function main(_) {
         if (_.__config.links.page.length) {
             const postBody = $('#cnblogs_post_body');
             const articleSuffixFlg = $('.articleSuffix-flg');
-            const linksHtml = _.__config.links.page.map((data) => generateSectionHtml(data, _)).join('');
+
+            // 生成友链的html
+            const generateLinkHtml = (link, index) => {
+                const { avatar = '', name = '', introduction = '', url = '' } = link;
+                const icons = ['icon-zhifeiji', 'icon-like_fill', 'icon-flashlight_fill'];
+                const icon = icons[index % icons.length];
+                return _.__tools.batchTempReplacement(linksTemp, [
+                    ['avatar', avatar],
+                    ['name', name],
+                    ['introduction', introduction],
+                    ['url', url],
+                    ['icon', icon],
+                ]);
+            };
+
+            // 生成完整的友链分类的html
+            const generateSectionHtml = (data) => {
+                const { title, icon, style, links } = data;
+                const sectionTitle = title ? `<h1 class="iconfont ${icon}" style="${style}">${title}</h1>` : '';
+                const linksHtml = links.map(generateLinkHtml).join('');
+                return `${sectionTitle}<div id="links-box">${linksHtml}</div>`;
+            };
+
+            const linksHtml = _.__config.links.page.map(generateSectionHtml).join('');
 
             // 插入模版
             articleSuffixFlg.length ? articleSuffixFlg.before(linksHtml) : postBody.append(linksHtml);
